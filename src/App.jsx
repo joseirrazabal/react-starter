8b057b6945511b11f0d50457c896a874d2cf4948
jirrazabal <joseirrazabal@aleani.com.ar>
2017-07-28 16:47:11 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import Prueba from './'
+import Componente from './'
 import styleGlobal from './css/index.scss'
 
 class App extends Component {
   render() {
     return (
-      <Prueba
+      <Componente 
         style={ styleGlobal }
       />
     )
